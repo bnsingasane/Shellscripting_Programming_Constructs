7dfee67cb01cd1b0cb58667d35317f965fcc3434
bhagyashree <bnsingasane@gmail.com>
2020-07-17 04:46:27 +0530

    Added:Solved Function Problems

---
+++ ProgrammingFunctions/pallindrome.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x 
+
+Pallindrome(){ 
+
+d=0;
+rev=" ";
+temp=$n;
+ 
+while [ $n -gt 0 ]
+do
+	d=$(( $n % 10 ))  
+	n=$(( $n / 10 ))  
+	rev=$( echo ${rev}${d} ) 
+done
+ 
+if [ $temp -eq $rev ];
+then
+	echo "Number is palindrome"
+else
+	echo "Number is NOT palindrome"
+fi
+}
+
+
+echo -n "Enter number:"
+read n
+Pallindrome $n
---
+++ ProgrammingFunctions/prime_pallindrome.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -x
+
+prime(){
+	counter=0;
+	for ((i=1 ; i<=n ; i++))
+	do
+		if [ $(($n % $i)) -eq 0 ]
+		then
+			counter++;
+		fi
+	done
+	if [ $counter -eq 1 ]
+	then
+		echo "Prime "
+	else
+		echo "Not prime"
+	fi
+}
+
+Pallindrome(){ 
+
+d=0;
+rev=" ";
+on=$n;
+ 
+while [ $n -gt 0 ]
+do
+    d=$(( $n % 10 ))  
+    n=$(( $n / 10 ))  
+    rev=$( echo ${rev}${d} ) 
+done
+ 
+if [ $on -eq $rev ];
+then
+  echo "Number is palindrome"
+else
+  echo "Number is NOT palindrome"
+fi
+}
+
+
+
+read -p "Enter number :" n
+prime $n 
