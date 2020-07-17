f9a5610aab2ddf4b3ac6d9ffb7c433c9dc655b8f
bhagyashree <bnsingasane@gmail.com>
2020-07-17 23:37:33 +0530

    Added:find same birth month of 50 individuals

--- ProgrammingDictionary/birth_month.sh
+++ ProgrammingDictionary/birth_month.sh
@@ -3,7 +3,6 @@
 month=(Jan Feb Mar April May June July Aug Sept Oct Nov Dec )
 echo "enter year"
 read year
-
 d= date +%y;
 
 if `[ $year -ge  93 ] && [$year -le  94 ]`
@@ -15,7 +14,7 @@ fi
 
 for ((i=1 ; i<=50 ; i++))
 do
-	Bmonth[$i]=${month[$((RANDOM % 12))]}
+	bmonth[$i]=${month[$((RANDOM % 12))]}
 done
 
 jan=0
@@ -33,40 +32,40 @@ dec=0
 
 for ((i=1 ; i<=50 ; i++))
 do
-	if [ ${Bmonth[$i]} == "January" ]
+	if [ ${bmonth[$i]} == "January" ]
 	then
 		jan=$(($jan + 1))
-	elif [ ${Bmonth[$i]} == "February" ]
+	elif [ ${bmonth[$i]} == "February" ]
 		then
 			feb=$(($feb + 1))
-		elif [ ${Bmonth[$i]} == "March" ]
+		elif [ ${bmonth[$i]} == "March" ]
 			then
 				march=$(($march + 1))
-			elif [ ${Bmonth[$i]} == "April" ]
+			elif [ ${bmonth[$i]} == "April" ]
                         then
                                 april=$(($april + 1))
-				elif [ ${Bmonth[$i]} == "May" ]
+				elif [ ${bmonth[$i]} == "May" ]
                         		then
                                 		may=$(($may + 1))
-					elif [ ${Bmonth[$i]} == "June" ]
+					elif [ ${bmonth[$i]} == "June" ]
                 			        then
 		                        	        june=$(($june + 1))
-						elif [ ${Bmonth[$i]} == "July" ]
+						elif [ ${bmonth[$i]} == "July" ]
                 				        then
 		                                		july=$(($july + 1))
-							elif [ ${Bmonth[$i]} == "August" ]
+							elif [ ${bmonth[$i]} == "August" ]
         					                then
 	                                				aug=$(($aug + 1))
-								elif [ ${Bmonth[$i]} == "September" ]
+								elif [ ${bmonth[$i]} == "September" ]
 						                        then
                                 				sept=$(($sept + 1))
-									elif [ ${Bmonth[$i]} == "October" ]
+									elif [ ${bmonth[$i]} == "October" ]
         							                then
 	                                						oct=$(($oct + 1))
-										elif [ ${Bmonth[$i]} == "November" ]
+										elif [ ${bmonth[$i]} == "November" ]
         								                then
 	                                							nov=$(($nov + 1))
-											elif [ ${Bmonth[$i]} == "December" ]
+											elif [ ${bmonth[$i]} == "December" ]
                         									then
 					                                				dec=$(($dec + 1))
 	fi
