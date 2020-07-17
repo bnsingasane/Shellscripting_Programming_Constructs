4e2178506d656e41cfae38f144ae9cbbe4179aba
bhagyashree <bnsingasane@gmail.com>
2020-07-17 23:51:47 +0530

    Added:display nth harnomic series

--- ProgrammingRepetition/ProgrammingForLoop/harmonic_series.sh
+++ ProgrammingRepetition/ProgrammingForLoop/harmonic_series.sh
@@ -3,11 +3,9 @@
 echo -p "Enter the value of n"
 read n
 sum=0;
-
 for ((i=1; i<=n; i++))
 do
 	s=`echo 'scale=2; 1/'$i' ' | bc `;
 	sum=`(echo $sum + $s  | bc )` ;
 done	
-
 echo "The sum of nth Harmonic is:$sum"
